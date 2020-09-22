0778ab876f5055907014108399737610d1477d4c
davebha <davebharat93@gmail.com>
2020-09-22 18:20:44 -0400

    Add the required methods in IBookService

--- ProjectSkeleton/Library.Services.Interface/IBookService.cs
+++ ProjectSkeleton/Library.Services.Interface/IBookService.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Library.DomainModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Library.Services.Interface
 {
     public interface IBookService
     {
+        Task<Book> AddBook(Author author, string title);
+        Task<IEnumerable<Book>> GetAllBooks();
 
     }
 }
