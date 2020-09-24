367dfac08748535800779e29f353261d9001409f
davebha <davebharat93@gmail.com>
2020-09-24 17:22:57 -0400

    Add BookRepository class to data access package and Add IBookRepository methods which throw NotImplementedException()

---
+++ ProjectSkeleton/Library.DataAccess/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Library.DataAccess.Interface;
+using Library.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess
+{
+    public class BookRepository : IBookRepository
+    {
+        public Task<Book> AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
