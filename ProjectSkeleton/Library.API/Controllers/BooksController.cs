208a8aa9ab720128d5832d7ef4c985f65344e77e
davebha <davebharat93@gmail.com>
2020-09-24 14:30:24 -0400

    Implement BooksController class with same methods as BookService which will receive data from this class.

--- ProjectSkeleton/Library.API/Controllers/BooksController.cs
+++ ProjectSkeleton/Library.API/Controllers/BooksController.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.DomainModels;
+using Library.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers
 {
     public class BooksController : ControllerBase
     {
-      
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpPost]
+        public async Task<Book> AddBook(int authorID,string title)
+        {
+
+            return await _bookService.AddBook(authorID, title);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetAllBooks()
+        {
+            return await _bookService.GetAllBooks();
+        }
+
+
+
+
     }
 }
