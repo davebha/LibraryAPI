0c0376a0e1025bd255841efecb26d575d831942e
davebha <davebharat93@gmail.com>
2020-10-02 16:46:45 -0400

    Change from using FindAsync() operator to using Where operator  to match the book title passed in and return it as list
    asynchronously.

--- ProjectSkeleton/Library.DataAccess/BookRepository.cs
+++ ProjectSkeleton/Library.DataAccess/BookRepository.cs
@@ -3,6 +3,7 @@ using Library.DomainModels;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,12 +33,23 @@ namespace Library.DataAccess
 
         public async Task<Book> GetBookById(int id)
         {
+
             return await _dbContext.Books.FindAsync(id);
         }
 
-        public async Task<Book> GetBookByName(string title)
+        public async Task<IEnumerable<Book>> GetBookByName(string title)
         {
-            return await _dbContext.Books.FindAsync(title);
+            //does not work here
+            // return await _dbContext.Books.FindAsync(title);
+
+            /*First => First element(at least 1 in list)
+             * Throws exception if empty
+             * FirstOrDefault => Like first method but brings designated "default"(i.e. null) value if empty
+             */
+            return await _dbContext.Books.Where(b => b.Title.Contains(title)).ToListAsync();
+                //FirstAsync();
         }
+
+        //POST,DELETE
     }
 }
