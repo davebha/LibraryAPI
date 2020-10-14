0553b4063613893d7d820e777685dea6b05e3859
davebha <davebharat93@gmail.com>
2020-10-14 15:49:22 -0400

    Implement AddSqlServerDbContext location for migrations to take place

--- ProjectSkeleton/Library.Utils/Extensions/LibraryRepositoryExtensions.cs
+++ ProjectSkeleton/Library.Utils/Extensions/LibraryRepositoryExtensions.cs
@@ -2,6 +2,7 @@
 using Library.DataAccess.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,17 @@ namespace Library.Utils.Extensions
             }
 
             return services;
+        }
+
+        public static IServiceCollection AddSqlServerDbContext(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            services.AddDbContext<LibraryDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("LibraryDbContextConnStr"), 
+                x=>x.MigrationsAssembly("Library.DataAccess")));
+
+            return services;
+            
         }
     }
 }
