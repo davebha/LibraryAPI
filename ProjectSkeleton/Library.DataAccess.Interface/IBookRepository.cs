04d8667e7fcbbf2430f2d30c32dad7a3decda40c
davebha <davebharat93@gmail.com>
2020-10-15 16:25:05 -0400

    Add GetBookBetween,UpdateBook methods and RemoveBook methods in BookRepository interface

--- ProjectSkeleton/Library.DataAccess.Interface/IBookRepository.cs
+++ ProjectSkeleton/Library.DataAccess.Interface/IBookRepository.cs
@@ -10,8 +10,35 @@ namespace Library.DataAccess.Interface
     {
         Task<Book> AddBook(Book book);
 
+
         Task<IEnumerable<Book>> GetAllBooks();
+
+
         Task<IEnumerable<Book>> GetBookByName(string title);
+
+
+
         Task<Book> GetBookById(int id);
+
+
+        Task<IEnumerable<Book>> GetBookBetween(DateTime startDate, DateTime endDate);
+
+
+
+        Task<Book> UpdateBook(Book book);
+
+      
+        Task RemoveBook(Book book);
+
+
+        //IEnumerable<Book> books
+
+        Task RemoveAllBooks();
+
+       
+        
+
+       
+       
     }
 }
