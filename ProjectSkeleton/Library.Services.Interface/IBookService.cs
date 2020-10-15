559304639273a8a74e2fe9c73d33feacbabd17fc
davebha <davebharat93@gmail.com>
2020-10-15 16:20:25 -0400

    Add GetBookBetween Read method and Update,Delete methods

--- ProjectSkeleton/Library.Services.Interface/IBookService.cs
+++ ProjectSkeleton/Library.Services.Interface/IBookService.cs
@@ -8,9 +8,24 @@ namespace Library.Services.Interface
 {
     public interface IBookService
     {
+        //CREATE
         Task<Book> AddBook(string title, int authorId);
+
+        //READ
         Task<IEnumerable<Book>> GetAllBooks();
         Task<Book> GetBookById(int id);
         Task<IEnumerable<Book>> GetBookByName(string title);
+        Task<IEnumerable<Book>> GetBookBetween(DateTime startDate,DateTime endDate);
+
+
+        //UPDATE
+        Task<Book> UpdateBookTitle(string newTitle);
+        Task<Book> UpdateIsAvailable(bool newStatus);
+
+        //DELETE
+        Task RemoveBook(int id);
+        Task RemoveAllBooks();
+
+        //IEnumerable<Book> books
     }
 }
